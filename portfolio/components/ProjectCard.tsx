917aa4776440df2199d9b0458a2eed5090e2735a
tiffany <ting11222001@gmail.com>
2023-02-03 22:55:41 +1030

    feat: 🎸 ProjectCard layout and add links

--- portfolio/components/ProjectCard.tsx
+++ portfolio/components/ProjectCard.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
-type Props = {}
 
-const ProjectCard = (props: Props) => {
+type Props = {
+  title: string,
+  description: string
+}
+
+const ProjectCard = ({ title, description }: Props) => {
   return (
-    <div>ProjectCard</div>
+    <div className="flex gap-4 pb-6">
+      <div className="flex-1 h-14 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg"></div>
+      <div className="flex-1">
+        <h3 className="font-medium">{title}</h3>
+        <p className="my-4 font-light">
+          {description}
+        </p>
+        <p>
+          <Link href="/">Read more →</Link>
+        </p>
+      </div>
+    </div>
   )
 }
 
--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Header, ProjectCard } from '../components';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -12,19 +13,22 @@ export default function Home() {
           <div className="py-8">
             <h1 className="font-display text-5xl py-8">A software developer passionate about building beautiful, user-friendly web and mobile applications</h1>
             <p className="text-2xl">
-              👋 Hi I'm Li-Ting, a design-focused developer based in Adelaide, Australia.
+              👋 Hi I am Li-Ting, a design-focused developer based in Adelaide, Australia.
             </p>
             <p className="text-xl pt-8">
-              View Profile  →
+              <Link href="/">View Profile →</Link>
             </p>
           </div>
 
           <div>
-            <h2 className="font-semibold text-2xl">Selected work</h2>
+            <h2 className="font-semibold text-2xl pb-8">Selected work</h2>
           </div>
-          <div>
-            <ProjectCard />
-          </div>
+          <ProjectCard title={"Metaverse"} description={"A metaverse-themed landing page with animation effect and responsive design."} />
+          <ProjectCard title={"The Succulent's World"} description={"An eCommerce shop for the Succulent's lovers."} />
+          <ProjectCard title={"Uber-Clone"} description={"Build Uber app front-end mobile with key features from scratch."} />
+          <p className="text-xl pt-8">
+            <Link href="/">View Projects →</Link>
+          </p>
         </div>
       </div>
     </body >
