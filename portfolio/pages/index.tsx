cae8d7a095ffb976aa23874be346c5e424bde367
tiffany <ting11222001@gmail.com>
2023-02-03 19:56:11 +1030

    feat: 🎸 profile section fonts

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -11,16 +11,16 @@ export default function Home() {
         <div className="flex flex-col max-w-screen-sm">
           <div className="py-8">
             <h1 className="font-display text-5xl py-8">A software developer passionate about building beautiful, user-friendly web and mobile applications</h1>
-            <div>
-              <p className="text-2xl">
-                👋 Hi I'm Li-Ting, a design-focused developer based in Adelaide, Australia.
-              </p>
-            </div>
+            <p className="text-2xl">
+              👋 Hi I'm Li-Ting, a design-focused developer based in Adelaide, Australia.
+            </p>
+            <p className="text-xl pt-8">
+              View Profile  →
+            </p>
           </div>
 
-
           <div>
-            <h2>Selected work</h2>
+            <h2 className="font-semibold text-2xl">Selected work</h2>
           </div>
         </div>
       </div>
