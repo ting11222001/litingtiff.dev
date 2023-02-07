999d6a028f54bdc4da92f45173dbf214a37c863b
tiffany <ting11222001@gmail.com>
2023-02-07 23:41:10 +1030

    feat: 🎸 add profile info

--- portfolio/constants/index.tsx
+++ portfolio/constants/index.tsx
@@ -16,3 +16,34 @@ export const projectsData = [
     demo: 'https://the-succulent-world-e-commerce.vercel.app/'
   },
 ];
+
+export const timeline = [
+  {
+    year: '2022 - Present',
+    title: 'Current Student at The University of Adelaide',
+    duration: '8 months',
+    details:
+      'I decided to pursue a Master\'s degree in Computing and Innovation at The University of Adelaide to sharpen my knowledge of Computer Science.I learned about computer systems, data structures and algorithms, and also built up fluency in multiple programming languages.',
+  },
+  {
+    year: '2020 - 2022',
+    title: 'Junior Developer',
+    duration: '1.5 year',
+    details:
+      'I was fortunate to be able to join Trunk Studio as a junior developer, working with the product team to create the front end of websites and mobile apps. We worked in an agile team setting. Our tech stack consisted of TypeSciprt, React, Vue, PHP, and a handful of other languages and tools.',
+  },
+  {
+    year: '2019 - 2020',
+    title: 'Amateur',
+    duration: '1 year',
+    details:
+      'While maintaining a demanding full-time job in the market research industry, I delved into various online and in-person programming courses. To document my coding experiences, I began sharing them on Medium and created a collection of portfolios.',
+  },
+  {
+    year: '2018',
+    title: 'First Line of Code',
+    duration: 'The beginning',
+    details:
+      'I wrote my first line of code that started this journey into the software development world I never could\'ve imagined. In 2018, I attended a workshop hosted by Women Who Code in Taipei (Taiwan). I was thrilled to discover how to write a program for a small Arduino device to play a Christmas carol. Since then, I have embarked on an amazing voyage into the world of coding.',
+  },
+]
