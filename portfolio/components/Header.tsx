0f8c370b788b4822158adc05ad8f48e0f4d102c8
tiffany <ting11222001@gmail.com>
2023-02-07 20:27:53 +1030

    fix: 🐛 sublink text size

--- portfolio/components/Header.tsx
+++ portfolio/components/Header.tsx
@@ -7,19 +7,19 @@ const Header = (props: Props) => {
   return (
     <div className="flex mx-auto max-w-xl justify-between pt-5">
       <div>
-        <h2>
+        <h2 className="text-xl">
           <Link href="/">Li-Ting Liao</Link>
         </h2>
-      </div>
+      </div >
 
       <div className="flex flex-row gap-2">
-        <h2>Profile</h2>
-        <h2>
+        <h2 className="text-xl">Profile</h2>
+        <h2 className="text-xl">
           <Link href="/projects">Projects</Link>
         </h2>
-        <h2>Contact</h2>
+        <h2 className="text-xl">Contact</h2>
       </div>
-    </div>
+    </div >
   )
 }
 
