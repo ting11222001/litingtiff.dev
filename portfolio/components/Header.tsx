c90720aa19ce644d669d81fdc46d4c64712579e0
tiffany <ting11222001@gmail.com>
2023-02-07 20:18:18 +1030

    feat: 🎸 create projects page

--- portfolio/components/Header.tsx
+++ portfolio/components/Header.tsx
@@ -7,12 +7,16 @@ const Header = (props: Props) => {
   return (
     <div className="flex mx-auto max-w-xl justify-between pt-5">
       <div>
-        Li-Ting Liao
+        <h2>
+          <Link href="/">Li-Ting Liao</Link>
+        </h2>
       </div>
 
       <div className="flex flex-row">
         <h2>Profile</h2>
-        <h2>Projects</h2>
+        <h2>
+          <Link href="/projects">Projects</Link>
+        </h2>
         <h2>Contact</h2>
       </div>
     </div>
---
+++ portfolio/pages/projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Header, ProjectCard } from '../components';
+
+type Props = {}
+
+const projects = (props: Props) => {
+  return (
+    <Header />
+  )
+}
+
+export default projects
