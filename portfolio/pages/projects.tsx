7d4394f7d261aafc13f10037dd1f3c5b144ab367
tiffany <ting11222001@gmail.com>
2023-02-07 20:28:03 +1030

    fix: 🐛 layout

--- portfolio/pages/projects.tsx
+++ portfolio/pages/projects.tsx
@@ -5,7 +5,18 @@ type Props = {}
 
 const projects = (props: Props) => {
   return (
-    <Header />
+    <div>
+      <Header />
+
+      <div className="flex flex-col mx-auto max-w-xl pt-16">
+        <h1 className="font-display text-5xl py-8">Projects</h1>
+
+        <p className="text-2xl">
+          🔨 Recent and past projects
+        </p>
+      </div>
+
+    </div>
   )
 }
 
