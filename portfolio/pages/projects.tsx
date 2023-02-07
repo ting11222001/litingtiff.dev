aefef84e9b7a25493e819c5412b7bb1df754fb17
tiffany <ting11222001@gmail.com>
2023-02-07 20:48:12 +1030

    feat: 🎸 add project cards

--- portfolio/pages/projects.tsx
+++ portfolio/pages/projects.tsx
@@ -11,9 +11,12 @@ const projects = (props: Props) => {
       <div className="flex flex-col mx-auto max-w-xl pt-16">
         <h1 className="font-display text-5xl py-8">Projects</h1>
 
-        <p className="text-2xl">
+        <p className="text-2xl mb-[5%]">
           🔨 Recent and past projects
         </p>
+
+        <ProjectCard image={"/metaverse.png"} title={"Metaverse"} description={"A metaverse-themed landing page with animation effect and responsive design."} />
+        <ProjectCard image={"/succulent.png"} title={"The Succulent's World"} description={"An eCommerce shop for the Succulent's lovers."} />
       </div>
 
     </div>
