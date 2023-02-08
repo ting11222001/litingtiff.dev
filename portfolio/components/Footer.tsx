f6369b84ab4102ef23052f32b94dd4d074daab66
tiffany <ting11222001@gmail.com>
2023-02-08 13:45:36 +1030

    fix: 🐛 footer color

--- portfolio/components/Footer.tsx
+++ portfolio/components/Footer.tsx
@@ -4,8 +4,8 @@ type Props = {}
 
 function Footer({ }: Props) {
   return (
-    <div className="flex mx-auto max-w-xl justify-between mt-10 border-t border-indigo-500">
-      <p className="text-sm mt-5 text-slate-500">© Copyright 2023 Li-Ting Liao. All rights reserved.</p>
+    <div className="flex mx-auto max-w-xl justify-between mt-10 border-t border-slate-500">
+      <p className="text-sm mt-5 text-slate-400">© Copyright 2023 Li-Ting Liao. All rights reserved.</p>
     </div>
   )
 }
