7f64a099254a9fa801b2a9e56815c6be9a4e90cf
tiffany <ting11222001@gmail.com>
2023-03-13 23:19:03 +1030

    fix: 🐛 mobile layout

--- portfolio/components/ProjectCard.tsx
+++ portfolio/components/ProjectCard.tsx
@@ -12,37 +12,79 @@ type Props = {
 const ProjectCard = ({ image, title, description, github, demo }: Props) => {
   console.log(github);
   return (
-    <div className="flex gap-4 pb-6">
-      <div className="flex-1 relative h-40 aspect-w-1 aspect-h-1 w-80 overflow-hidden rounded-md">
-        <img
-          src={image}
-          alt="project image"
-          className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-        />
-      </div>
-      <div className="flex-1">
-        <h3 className="font-medium">{title}</h3>
-        <p className="my-4 font-light">
-          {description}
-        </p>
+    <div className='w-full mx-auto'>
+      <div className='md:flex md:pb-4 items-center'>
 
-        <div className="flex flex-row gap-4">
-          {github && (
-            <div className="border-solid border-2 border-slate-500 p-1 rounded-md hover:border-pink-500">
-              <a href={github}>
-                GitHub
+        <div className="relative h-40 aspect-w-1 aspect-h-1 md:w-[300px] overflow-hidden rounded-md">
+          <img
+            src={image}
+            alt="project image"
+            className="h-full w-full object-cover object-center"
+          />
+        </div>
+
+        <div className='max-w-[300px] py-4 md:pl-4 md:py-0'>
+          <h3 className="font-medium">{title}</h3>
+          <p className="my-4 font-light">
+            {description}
+          </p>
+
+
+          <div className='flex space-x-4'>
+            {github && (
+              <div className="border-solid border-2 border-slate-500 p-1 rounded-md hover:border-pink-500">
+                <a href={github}>
+                  GitHub
+                </a>
+              </div>
+            )}
+
+            <div className="border-solid border-2 border-slate-500 p-1 rounded-md hover:border-yellow-500">
+              <a href={demo}>
+                Demo
               </a>
             </div>
-          )}
-          <div className="border-solid border-2 border-slate-500 p-1 rounded-md hover:border-yellow-500">
-            <a href={demo}>
-              Demo
-            </a>
           </div>
         </div>
 
-      </div>
-    </div>
+      </div >
+
+
+
+    </div >
+
+
+    // <div className="flex gap-4 pb-6">
+    //   <div className="flex-1 relative h-40 aspect-w-1 aspect-h-1 w-80 overflow-hidden rounded-md">
+    //     <img
+    //       src={image}
+    //       alt="project image"
+    //       className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+    //     />
+    //   </div>
+    //   <div className="flex-1">
+    //     <h3 className="font-medium">{title}</h3>
+    //     <p className="my-4 font-light">
+    //       {description}
+    //     </p>
+
+    //     <div className="flex flex-row gap-4">
+    //       {github && (
+    //         <div className="border-solid border-2 border-slate-500 p-1 rounded-md hover:border-pink-500">
+    //           <a href={github}>
+    //             GitHub
+    //           </a>
+    //         </div>
+    //       )}
+    //       <div className="border-solid border-2 border-slate-500 p-1 rounded-md hover:border-yellow-500">
+    //         <a href={demo}>
+    //           Demo
+    //         </a>
+    //       </div>
+    //     </div>
+
+    //   </div>
+    // </div>
   )
 }
 
