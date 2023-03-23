c293b4b4202b11791675ceff8f5e0d1e25460e1f
tiffany <ting11222001@gmail.com>
2023-03-24 00:00:15 +1030

    fix: 🐛 small mobile

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="flex mx-auto max-w-xl pt-16 relative">
         <div className="flex items-center absolute opacity-40 top-0 pt-16">
           <div className="relative md:top-[-30px] top-0 left-0 w-40 h-40 rounded-full bg-gradient-to-b from-violet-500 to-fuchsia-500 z-40 animate-spin-slow"></div>
-          <div className="relative md:top-0 top-[40px] left-[-50px] w-80 h-80 rounded-full bg-gradient-to-b from-green-400 to-blue-500 z-30 animate-spin-slow"></div>
+          <div className="relative md:top-0 top-[80px] left-[-50px] w-60 h-60 md:w-80 md:h-80 rounded-full bg-gradient-to-b from-green-400 to-blue-500 z-30 animate-spin-slow"></div>
           <div className="self-end relative top-0 left-[-120px] md:w-60 md:h-60 rounded-full bg-gradient-to-b from-pink-500 to-yellow-500 z-20 animate-spin-slow"></div>
         </div>
 
